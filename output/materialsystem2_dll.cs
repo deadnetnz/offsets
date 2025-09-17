52c6cd1eaec685280e3c5870630d55c8ca0c1efe
idanoo <daniel@m2.nz>
2025-09-17 18:56:50 +1200

    2025-09-17

--- output/materialsystem2_dll.cs
+++ output/materialsystem2_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2025-09-12 06:45:44.515762600 UTC
+// 2025-09-17 06:56:32.031354 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: materialsystem2.dll
